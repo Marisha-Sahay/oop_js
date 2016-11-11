55cdd0f7062fc58a84cfc9e250cd81d7f93ad33c
Marisha Sahay <marishasahay@gmail.com>
2016-11-10 19:10:10 -0800

    Add colors for boxes

--- javascript.js
+++ javascript.js
@@ -1,13 +1,35 @@
-function createAccount() {
-  var firstName = document.getElementById('firstName');
-  var lastName = document.getElementById('lastName');
-  var openingBalance = document.getElementById('openingBalance');
-  var account = new Account(firstName.value, lastName.value, openingBalance.value); // need to create constructor
-  accounts.push(account);
-  firstName.value = "";
-  lastName.value = "";
-  openingBalance.value = "";
-}
+var clickBoxOne = (function() {
+  var colors = ["red", "black", "green"];
+    var i = 0;
+    return function() {
+      if(i === 3) {
+      i = 0;
+      }
+    box1.style.background = colors[i];
+    i ++;        
+    }
+})();
 
-window.accounts = accounts;
+var clickBoxTwo = (function() {
+  var colors = ["red", "black", "green"];
+    var i = 0;
+    return function() {
+    box2.style.background = colors[i%3];
+    i ++;        
+    }
+})();
+
+
+// function createAccount() {
+//   var firstName = document.getElementById('firstName');
+//   var lastName = document.getElementById('lastName');
+//   var openingBalance = document.getElementById('openingBalance');
+//   var account = new Account(firstName.value, lastName.value, openingBalance.value); // need to create constructor
+//   accounts.push(account);
+//   firstName.value = "";
+//   lastName.value = "";
+//   openingBalance.value = "";
+// }
+
+// window.accounts = accounts;
 
